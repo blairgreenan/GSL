a5a3219c4ee7a84b6802ce3b3b37e5e4ed8162a6
Greenan <Blair.Greenan@dfo-mpo.gc.ca>
2024-12-22 19:54:38 -0400

    Updated figure style

--- GSL_S.R
+++ GSL_S.R
@@ -6,15 +6,15 @@ Sal <- read.table("GSL_S.dat", header = TRUE, sep = "", col.names = c("Year", "D
 ggpS <- ggplot(Sal) + geom_line(aes(x=Year, y=D150m), color = "black", linewidth = 1.0) +
   geom_point(aes(x=Year, y=D150m), color = "black") +
   geom_smooth(aes(x=Year, y=D150m), method = "lm", se = TRUE, color = "black") +
-  geom_label(aes(x=2028, y=33.3, label = "150 m"), color = "black")
+  geom_label(aes(x=2020, y=33.1, label = "150 m"), color = "black")
 ggpS <- ggpS + geom_line(aes(x=Year, y=D200m), color = "red")  +
   geom_point(aes(x=Year, y=D200m), color = "red")  +
   geom_smooth(aes(x=Year, y=D200m), method = "lm", se = TRUE, color = "red") +
-  geom_label(aes(x=2028, y=34.1, label = "200 m"), color = "red")
+  geom_label(aes(x=2020, y=33.9, label = "200 m"), color = "red")
 ggpS <- ggpS + geom_line(aes(x=Year, y=D300m), color = "blue") +
   geom_point(aes(x=Year, y=D300m), color = "blue") +
   geom_smooth(aes(x=Year, y=D300m), method = "lm", se = TRUE, color = "blue") +
-  geom_label(aes(x=2028, y=34.8, label = "300 m"), color = "blue")
+  geom_label(aes(x=2020, y=34.5, label = "300 m"), color = "blue")
 ggpS <- ggpS + ylab(expression("Layer Average Salinity")) + xlab("Year")
 
 # fit a linear model to the anomaly data
@@ -32,4 +32,4 @@ confidence_95_300 <- confint(model_300, level = 0.95)
 
 
 # ggsave("GSL.png", scale = 1)
-ggsave("GSL_S_with_lm.png", width = 6, height = 3, units = "in", scale = 1.5, dpi = 1200)
+ggsave("GSL_S_with_lm.png", width = 6, height = 3, units = "in", scale = 1.0, dpi = 1200)
